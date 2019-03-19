68be40bfaa1232ea7e1413ffcd1633fcf7eb178d
Markus <markusvaikjarv@github.com>
2019-03-20 01:12:07 +0200

    corrected formatting

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import {IndexController, UsersController} from './controllers/Controllers';
+import {IndexController, UsersController} from './controllers/Controllers'
 import {CreateUsersTable} from './database/DbOperations'
 
 //Setup
